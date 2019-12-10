28ac66062f128fbb6fa48cd14d9f7b8d985f0b01
Young Won <youngw417@gmail.com>
2019-12-09 20:35:30 -0800

    shopping cart mvp with stretch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -10,31 +11,63 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
+	// get cart when refresh
+	useEffect(() => {
+	if (window.localStorage.cart) 
+
+		setCart(JSON.parse(window.localStorage.cart))
+	
+	
+	}, []);
+
+	useEffect(() => {
+		if (cart) 
+			window.localStorage.setItem('cart',JSON.stringify(cart));
+		
+		}, [cart]);
+
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart,item])
 	};
+
+	const removeItem = item => {
+		
+		setCart(cart.filter(each => item.id !== each.id))
+		
+	}
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			
+			<ProductContext.Provider value = { {
+				products,
+		
+				addItem
+			}}>
+				<CartContext.Provider value = {{
+					cart,
+					removeItem }} >
+					<Navigation />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+							
+					<Route
+					exact
+					path="/"
+				
+					component = {Products}
+				
 					/>
-				)}
-			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+					<Route
+						path="/cart"
+						component = {ShoppingCart}
+					/>
+				</CartContext.Provider>
+				
+			</ProductContext.Provider>
+			
 		</div>
 	);
 }
